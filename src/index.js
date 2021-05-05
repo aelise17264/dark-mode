ed2a031cb9ae486754913491ad429e811d30d954
unknown <aelise17264@gmail.com>
2021-05-04 20:07:23 -0600

    updated toggle button and title

--- src/index.js
+++ src/index.js
@@ -28,11 +28,14 @@ const App = () => {
     <div className={darkMode ? "dark-mode App" : "App"}>
      <nav className="navbar">
       <h1>Crypto Tracker</h1>
+      <div className="title">
+      <h4>Toggle for Dark Mode</h4>
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
           className={darkMode ? 'toggle toggled' : 'toggle'}
         />
+      </div>
       </div>
     </nav>
       <Charts coinData={coinData} />
